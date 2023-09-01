5a3f089e78d137879e381c700cf98f68be267468
ckdev88 <ckdev88@outlook.com>
2023-09-01 08:20:25 -0300

    add typescript

--- src/supabase.ts
+++ src/supabase.ts
@@ -4,4 +4,5 @@ import type { Database } from './types'
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+export default supabase
