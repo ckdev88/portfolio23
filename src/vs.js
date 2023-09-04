3c7d305f1963be1f9b27b6be627d36c5793d374b
ckdev88 <ckdev88@outlook.com>
2023-09-04 02:15:56 -0300

    bit less code

--- src/vs.js
+++ src/vs.js
@@ -1,6 +1,6 @@
 // TODO: need to be either cleaned up or used, also see pages/nl/index.astro
 import supabase from "./supabase";
-const currentUrl="NLLLLL";
+const currentUrl = "NLLLLL";
 await supabase
   .rpc("count_visits", {
     url: currentUrl,
