f874902f45f46309e9ab744fe85d30a26d69dcd9
ckdev88 <ckdev88@outlook.com>
2023-09-04 01:28:56 -0300

    experimentals

---
+++ src/components/VisitorsSupabase.tsx
@@ -0,0 +1,10 @@
+import supabase from "../supabase";
+// werkt, maar lokaal en beetje ruk
+// const currentUrl = 'jajajaja9';
+// type InfoProps = {
+// 	currentUrl: string;
+// }
+// await supabase
+//   .rpc('count_visits', {
+// 		url:currentUrl
+//   }).single()
--- src/supabase.ts
+++ src/supabase.ts
@@ -5,4 +5,10 @@ const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+// TODO: remove or use, works only locally
+// await supabase
+//   .rpc('count_visits', {
+//     url: 'asdasd.href'
+//   }).single();
+
 export default supabase;
