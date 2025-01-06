12827af7a4200a77459b0b78bdf4c4299c95bb6a
ckdev88 <ckdev88@outlook.com>
2025-01-06 19:59:26 -0300

    change title andd some formatting

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,79 +1,79 @@
 export const languages = {
-  nl: "NL", //Nederlands
-  en: "EN", //English
-  // pt: "PT", //Português
+	nl: 'NL', //Nederlands
+	en: 'EN', //English
+	// pt: "PT", //Português
 };
 
-export const defaultLang = "en";
+export const defaultLang = 'en';
 
 export const ui = {
-  nl: {
-    "nav.Hi": "Hoi",
-    "nav.Future": "Toekomst",
-    "nav.Past": "Projecten",
-    "nav.Skills": "Stack",
-    "nav.Tools": "Tools",
-    "nav.Contact": "Contact",
-    "lt.JumpTo": "Spring naar",
-    "ui.Accessibility": "Toegankelijkheid",
-    "hi-h1": "Ik ben Cors,",
-    "hi-h1-sub": "Web consultant.",
-    "hi-p":
-      "Meer dan 15 jaar professionele ervaring als full stack online software ontwikkelaar, werkend aan digitale producten van promotionele websites tot enterprise webshops.",
-    "hi-p2":
-      "Gespecialiseerd in front-end ontwikkeling, met toegankelijkheid, performance en gebruikersgemak als prioriteiten, zodat het eindproduct zo inclusief mogelijk is.",
-    "past.head": "Mijn werk, projecten en werkgevers.",
-    "past.p":
-      "Een greep uit projecten waaraan ik heb gewerkt, met de daarbij horende vaardigheden.",
-    "past-1-h3": "",
-    "past-1-h3-sub": "",
-    "past-1-p":
-      "Took some time to take on various private projects, using a variety of frameworks and techniques.",
-    "past-2-h3": "Visma PlusPort",
-    "past-2-h3-sub": "2017 - 2021",
-    "past-2-p":
-      "Leading a small team responsible for the front-end and accessibility	(WCAG 2.1) of our main product: PlusPort LMS, as well as our own websites and other web services.",
-    "past-3-h3": "Clever Strategy",
-    "past-3-h3-sub": "2011 - 2017",
-    "past-3-p":
-      "Working as front-end developer and campaign manager, specialized in accountable marketing. Directly in contact with many clients, to fine tune the communication in running campaigns, where we made several flows to target and retarget the (potential) customers, to keep them engaged. The campaigns were a mix of physical and virtual, webshops, campaign-websites, e-mails and physical mail.",
-  },
-  en: {
-    "nav.Hi": "Hi",
-    "nav.Future": "Future",
-    "nav.Past": "Projects",
-    "nav.Skills": "Stack",
-    "nav.Tools": "Tools",
-    "nav.Contact": "Contact",
-    "lt.JumpTo": "Jump to",
-    "ui.Accessibility": "Accessibility",
-    "hi-h1": "Hi, my name is Cors,",
-    "hi-h1-sub": "Web consultant.",
-    "hi-p":
-      "I love to build stuff. For more than 15 years I have been building digital products for businesses ranging from simple promotional websites to enterprise webshops.",
-    "hi-p2":
-      "Specialized in front-end development, with accessibility, performance and ease of use as priorities, so the end result is as inclusive as possible, resulting in more reach, engagement, higher conversion rates, more sales.",
-    "past.head": "Some of my work.",
-    "past.p":
-      "Below you can see a selection of some of my employers, jobs, projects	and applied skills.",
-  },
-  pt: {
-    "nav.Hi": "Ola",
-    "nav.Future": "Futuro",
-    "nav.Past": "Projetos",
-    "nav.Skills": "Skills",
-    "nav.Tools": "Tools",
-    "nav.Contact": "Contato",
-    "nav.JumpTo": "Saltar para",
-    "ui.Accessibility": "Accessibility",
-    "hi-h1": "Ola, eu sou Cors,",
-    "hi-h1-sub": "Web consultant, senior web desenvolvedor.",
-    "hi-p":
-      "More than 15 years of professional experience building products for businesses ranging from simple promotional websites to enterprise webshops.",
-    "hi-p2":
-      "Specialized in front-end development, with accessibility, performance and ease of use as priorities, so the end result is as inclusive as possible.",
-    "past.head": "Some of my work.",
-    "past.p":
-      "Below you can see a selection of some of my employers, jobs, projects and applied skills.",
-  },
+	nl: {
+		'nav.Hi': 'Hoi',
+		'nav.Future': 'Toekomst',
+		'nav.Past': 'Projecten',
+		'nav.Skills': 'Stack',
+		'nav.Tools': 'Tools',
+		'nav.Contact': 'Contact',
+		'lt.JumpTo': 'Spring naar',
+		'ui.Accessibility': 'Toegankelijkheid',
+		'hi-h1': 'Ik ben Cors,',
+		'hi-h1-sub': 'Software engineer.',
+		'hi-p':
+			'Meer dan 15 jaar professionele ervaring als full stack online software ontwikkelaar, werkend aan digitale producten van promotionele websites tot enterprise webshops.',
+		'hi-p2':
+			'Gespecialiseerd in front-end ontwikkeling, met toegankelijkheid, performance en gebruikersgemak als prioriteiten, zodat het eindproduct zo inclusief mogelijk is.',
+		'past.head': 'Mijn werk, projecten en werkgevers.',
+		'past.p':
+			'Een greep uit projecten waaraan ik heb gewerkt, met de daarbij horende vaardigheden.',
+		'past-1-h3': '',
+		'past-1-h3-sub': '',
+		'past-1-p':
+			'Took some time to take on various private projects, using a variety of frameworks and techniques.',
+		'past-2-h3': 'Visma PlusPort',
+		'past-2-h3-sub': '2017 - 2021',
+		'past-2-p':
+			'Leading a small team responsible for the front-end and accessibility	(WCAG 2.1) of our main product: PlusPort LMS, as well as our own websites and other web services.',
+		'past-3-h3': 'Clever Strategy',
+		'past-3-h3-sub': '2011 - 2017',
+		'past-3-p':
+			'Working as front-end developer and campaign manager, specialized in accountable marketing. Directly in contact with many clients, to fine tune the communication in running campaigns, where we made several flows to target and retarget the (potential) customers, to keep them engaged. The campaigns were a mix of physical and virtual, webshops, campaign-websites, e-mails and physical mail.',
+	},
+	en: {
+		'nav.Hi': 'Hi',
+		'nav.Future': 'Future',
+		'nav.Past': 'Projects',
+		'nav.Skills': 'Stack',
+		'nav.Tools': 'Tools',
+		'nav.Contact': 'Contact',
+		'lt.JumpTo': 'Jump to',
+		'ui.Accessibility': 'Accessibility',
+		'hi-h1': 'Hi, my name is Cors,',
+		'hi-h1-sub': 'Software engineer.',
+		'hi-p':
+			'I love to build stuff. For more than 15 years I have been building digital products for businesses ranging from simple promotional websites to enterprise webshops.',
+		'hi-p2':
+			'Specialized in front-end development, with accessibility, performance and ease of use as priorities, so the end result is as inclusive as possible, resulting in more reach, engagement, higher conversion rates, more sales.',
+		'past.head': 'Some of my work.',
+		'past.p':
+			'Below you can see a selection of some of my employers, jobs, projects	and applied skills.',
+	},
+	pt: {
+		'nav.Hi': 'Ola',
+		'nav.Future': 'Futuro',
+		'nav.Past': 'Projetos',
+		'nav.Skills': 'Skills',
+		'nav.Tools': 'Tools',
+		'nav.Contact': 'Contato',
+		'nav.JumpTo': 'Saltar para',
+		'ui.Accessibility': 'Accessibility',
+		'hi-h1': 'Ola, eu sou Cors,',
+		'hi-h1-sub': 'Software engineer.',
+		'hi-p':
+			'More than 15 years of professional experience building products for businesses ranging from simple promotional websites to enterprise webshops.',
+		'hi-p2':
+			'Specialized in front-end development, with accessibility, performance and ease of use as priorities, so the end result is as inclusive as possible.',
+		'past.head': 'Some of my work.',
+		'past.p':
+			'Below you can see a selection of some of my employers, jobs, projects and applied skills.',
+	},
 } as const;
